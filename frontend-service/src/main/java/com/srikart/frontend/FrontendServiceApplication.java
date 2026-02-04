ecc37c358b93aeb48acead83ec8c047f57af7464
srikarimmidisetty <30678063+srikarimmidisetty@users.noreply.github.com>
2026-02-04 14:26:21 +0530

    Create FrontendServiceApplication for Spring Boot

---
+++ frontend-service/src/main/java/com/srikart/frontend/FrontendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.srikart.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FrontendServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendServiceApplication.class, args);
+    }
+}
