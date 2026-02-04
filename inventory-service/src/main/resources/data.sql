2cc8ee3430561ca5aad770d4c76f8307a0f1b1b9
srikarimmidisetty <30678063+srikarimmidisetty@users.noreply.github.com>
2026-02-04 14:18:07 +0530

    Insert vehicle data into the database

---
+++ inventory-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO vehicles (brand, model, type, year, price, quantity, description, image_url, engine_capacity, horse_power, top_speed, fuel_type, transmission) VALUES
+('BMW', 'M8 Competition', 'CAR', 2023, 146000.00, 5, 'Luxury sports coupe', 'https://images.unsplash.com/photo-1580273916550-e323be2ae537?w=800', '4.4L V8', 617, 250, 'PETROL', 'Automatic'),
+('Mercedes', 'S-Class', 'CAR', 2023, 114000.00, 8, 'Flagship luxury sedan', 'https://images.unsplash.com/photo-1562911791-4f7b0c11d1d1?w=800', '3.0L I6', 429, 155, 'HYBRID', 'Automatic'),
+('Audi', 'R8', 'CAR', 2023, 169000.00, 3, 'Mid-engine supercar', 'https://images.unsplash.com/photo-1565898835704-3d6be4a2c98b?w=800', '5.2L V10', 562, 205, 'PETROL', 'Automatic'),
+('Lamborghini', 'Aventador', 'CAR', 2023, 417000.00, 2, 'Extreme supercar', 'https://images.unsplash.com/photo-1507136566006-cfc505b114fc?w=800', '6.5L V12', 769, 217, 'PETROL', 'Automatic'),
+('Kawasaki', 'Ninja H2R', 'BIKE', 2023, 55000.00, 6, 'Track-only hyperbike', 'https://images.unsplash.com/photo-1558981285-6f0c94958bb6?w=800', '998cc', 310, 249, 'PETROL', '6-Speed'),
+('BMW', 'S 1000 RR', 'BIKE', 2023, 18900.00, 10, 'Superbike', 'https://images.unsplash.com/photo-1558981403-c5f9899a28bc?w=800', '999cc', 207, 186, 'PETROL', '6-Speed'),
+('Triumph', 'Rocket 3', 'BIKE', 2023, 23000.00, 7, 'Power cruiser', 'https://images.unsplash.com/photo-1568772585407-9361f9bf3a87?w=800', '2458cc', 165, 140, 'PETROL', '6-Speed'),
+('Ducati', 'Panigale V4', 'BIKE', 2023, 24500.00, 5, 'Italian superbike', 'https://images.unsplash.com/photo-1558981001-792f6c63d1e9?w=800', '1103cc', 214, 186, 'PETROL', '6-Speed');
